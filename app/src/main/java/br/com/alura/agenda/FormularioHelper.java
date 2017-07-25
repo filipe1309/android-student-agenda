c90bf6b949e3e14cb46c0343c504755968836a3f
filipe1309 <filipe1309@gmail.com>
2017-07-24 22:26:00 -0300

    Add verification for file existence

--- app/src/main/java/br/com/alura/agenda/FormularioHelper.java
+++ app/src/main/java/br/com/alura/agenda/FormularioHelper.java
@@ -6,11 +6,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 
+import java.io.File;
+
 import br.com.alura.agenda.modelo.Aluno;
 
-/**
- * Created by alura on 12/08/15.
- */
 public class FormularioHelper {
 
     private final EditText campoNome;
@@ -54,11 +53,14 @@ public class FormularioHelper {
 
     public void carregaImagem(String caminhoFoto) {
         if (caminhoFoto != null) {
-            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
-            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
-            campoFoto.setImageBitmap(bitmapReduzido);
-            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
-            campoFoto.setTag(caminhoFoto);
+            File file = new File(caminhoFoto);
+            if(file.exists()) {
+                Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+                Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+                campoFoto.setImageBitmap(bitmapReduzido);
+                campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+                campoFoto.setTag(caminhoFoto);
+            }
         }
     }
 }
