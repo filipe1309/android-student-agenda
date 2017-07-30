0e2e352119bb81eafac2c62bb660c2c6d876106b
filipe1309 <filipe1309@gmail.com>
2017-07-29 22:39:33 -0300

    Add service to send push notification to specific devices via token

--- app/src/main/java/br/com/alura/agenda/firebase/AgendaInstanceIdService.java
+++ app/src/main/java/br/com/alura/agenda/firebase/AgendaInstanceIdService.java
@@ -4,10 +4,10 @@ import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
-
-/**
- * Created by filipe1309 on 25/07/17.
- */
+import br.com.alura.agenda.retrofit.RetrofitInicializador;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AgendaInstanceIdService extends FirebaseInstanceIdService {
     @Override
@@ -22,7 +22,19 @@ public class AgendaInstanceIdService extends FirebaseInstanceIdService {
         enviaTokenParaServidor(refreshedToken);
     }
 
-    private void enviaTokenParaServidor(String refreshedToken) {
+    private void enviaTokenParaServidor(final String token) {
+        Call<Void> call = new RetrofitInicializador().getDispositivoService().enviaToken(token);
 
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Log.i("token enviado", token);
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+
+            }
+        });
     }
 }
