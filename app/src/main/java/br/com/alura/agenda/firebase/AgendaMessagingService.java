ab977fd2d700a0108650e2bff2cb7f28883f2a0b
filipe1309 <filipe1309@gmail.com>
2017-07-31 20:30:57 -0300

    Convert info send by server into student objects

--- app/src/main/java/br/com/alura/agenda/firebase/AgendaMessagingService.java
+++ app/src/main/java/br/com/alura/agenda/firebase/AgendaMessagingService.java
@@ -2,9 +2,14 @@ package br.com.alura.agenda.firebase;
 
 import android.util.Log;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.IOException;
 import java.util.Map;
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.dto.AlunoSync;
 
 public class AgendaMessagingService extends FirebaseMessagingService {
 
@@ -14,5 +19,23 @@ public class AgendaMessagingService extends FirebaseMessagingService {
 
         Map<String, String> mensagem = remoteMessage.getData();
         Log.i("mensagem recebida", String.valueOf(mensagem));
+
+        converteParaAluno(mensagem);
+    }
+
+    private void converteParaAluno(Map<String, String> mensagem) {
+        String chaveDeAcesso = "alunoSync";
+        if (mensagem.containsKey(chaveDeAcesso)) {
+            String json = mensagem.get(chaveDeAcesso);
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                AlunoSync alunoSync = mapper.readValue(json, AlunoSync.class);
+                AlunoDAO alunoDAO = new AlunoDAO(this);
+                alunoDAO.sincroniza(alunoSync.getAlunos());
+                alunoDAO.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
